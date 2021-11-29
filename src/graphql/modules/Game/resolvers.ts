79a27ccf9e6f3808ad6075ccbe87b9120d339747
P37R0L4 <lucao.gadget@gmail.com>
2021-11-28 23:54:11 -0300

    Minigame with points updated

--- src/graphql/modules/Game/resolvers.ts
+++ src/graphql/modules/Game/resolvers.ts
@@ -8,11 +8,10 @@ export default {
   },
   Mutation: {
     createGame: async (_: any, { data }: any) => await Game.create(data),
-    updateGame: async (_: any, { id, data }: any) => {
-      const dataUpdated = await Game.findOneAndUpdate(id, data, { new: true });
-
-      return dataUpdated;
-    },
+    updateGame: async (_: any, { id, data }: any) =>
+      await Game.findOneAndUpdate({ idRecruiter: id }, data, {
+        new: true,
+      }),
     deleteGame: async (_: any, { id }: any) =>
       !!(await Game.findOneAndDelete(id)),
   },
