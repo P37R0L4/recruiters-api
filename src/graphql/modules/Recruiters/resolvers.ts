392135524416a7467da53daead916a77528e7188
P37R0L4 <lucao.gadget@gmail.com>
2021-11-30 19:47:56 -0300

    teste heroku 2

--- src/graphql/modules/Recruiters/resolvers.ts
+++ src/graphql/modules/Recruiters/resolvers.ts
@@ -2,15 +2,7 @@ import Recruiters from '../../../models/recruiters';
 
 export default {
   Query: {
-    recruiters: async () => {
-      try {
-        const data = await Recruiters.find();
-        console.log(data);
-        return await data;
-      } catch (err) {
-        console.log(err);
-      }
-    },
+    recruiters: async () => await Recruiters.find(),
     recruiter: async (_: any, { id }: any) => await Recruiters.findById(id),
   },
   Mutation: {
